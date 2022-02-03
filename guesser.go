de0d88f0b5801747fd1de46df1eca4cc81fe7dd3
Horatiu Ion <horatiu@findhotel.net>
2022-02-03 15:44:54 +0100

    some guesser fixes

--- guesser.go
+++ guesser.go
@@ -2,15 +2,15 @@ package main
 
 type Guesser struct {
 	chars   [5]byte
-	charMap map[byte]struct{}
+	charMap map[byte]int
 }
 
 func NewGueser(word string) *Guesser {
 	g := &Guesser{
-		charMap: map[byte]struct{}{},
+		charMap: map[byte]int{},
 	}
 	for i := range word {
-		g.charMap[word[i]] = struct{}{}
+		g.charMap[word[i]]++
 		g.chars[i] = word[i]
 	}
 	return g
@@ -21,6 +21,10 @@ func (g *Guesser) Guess(word string) (result guessResult) {
 		char := word[i]
 		if g.chars[i] == char {
 			result[i] = DirectHit
+			g.charMap[char]--
+			if g.charMap[char] == 0 {
+				delete(g.charMap, char)
+			}
 			continue
 		}
 		if _, found := g.charMap[char]; found {
